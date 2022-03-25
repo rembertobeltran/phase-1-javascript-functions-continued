0fd3a3d3c7de6ffb7a3bcb35098b7b2ecd9f4529
Remberto <rembertobeltran@icloud.com>
2022-03-25 18:26:29 -0400

    Complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(funActivity = 'roller-skate') {
+    return 'This Saturday, I want to ' + funActivity + '!'
+}
+
+saturdayFun()
+saturdayFun('bathe my dog')
+
+function mondayWork(workActivity = 'go to the office') {
+    return 'This Monday, I will ' + workActivity +'.'
+}
+
+mondayWork()
+mondayWork('work from home')
+
+function wrapAdjective(highLight){
+    const intro = 'You are '
+    const outro = '!'
+    return function(empathic)  {
+       return intro + highLight + empathic + highLight + outro
+       }   
+}
+
+wrapAdjective('*')(' a hard worker')
+wrapAdjective('||')('a dedicated programmer')
+
